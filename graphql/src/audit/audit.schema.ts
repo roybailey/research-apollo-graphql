5f011b20f702f72f006f6e55dbef3e67f4d4832f
Roy Bailey <roybaileybiz@gmail.com>
2022-01-26 17:04:40 +0000

    attempt to upgrade packages

--- graphql/src/audit/audit.schema.ts
+++ graphql/src/audit/audit.schema.ts
@@ -41,7 +41,7 @@ export const resolvers:IResolvers = {
 
   Subscription: {
     event: {
-      subscribe: (parent, args, { pubsub }) => {
+      subscribe: (parent:any, args:any, { pubsub }:any) => {
         const channel = Math.random().toString(36).substring(2, 15) // random channel name
         const auditObserver = {
           next(events:IAudit[]) {
